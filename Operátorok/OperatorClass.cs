8583b3c43d429491fe658830cef3491fec4a138a
kytrack <vkrisztianmgy@gmail.com>
2023-05-24 21:42:07 +0200

    kész

---
+++ Operátorok/OperatorClass.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operátorok
+{
+    class OperatorClass
+    {
+        int operandus1;
+        string operatorhatorozo;
+        int operandus2;
+
+        public OperatorClass(int operandus1, string operatorhatorozo, int operandus2)
+        {
+            this.operandus1 = operandus1;
+            this.operatorhatorozo = operatorhatorozo;
+            this.operandus2 = operandus2;
+        }
+
+        public int Operandus1 { get => operandus1; }
+        public string Operatorhatorozo { get => operatorhatorozo; }
+        public int Operandus2 { get => operandus2; }
+
+
+        public static string Meghatarozas(int operandus1, string operatorhatorozo, int operandus2)
+        {
+            try
+            {
+                if (operatorhatorozo == "+")
+                {
+                    return (operandus1 + operandus2).ToString();
+                }
+                else if (operatorhatorozo == "-")
+                {
+                    return (operandus1 - operandus2).ToString();
+                }
+                else if (operatorhatorozo == "*")
+                {
+                    return (operandus1 * operandus2).ToString();
+                }
+                else if (operatorhatorozo == "/")
+                {
+                    return (operandus1 / (double)operandus2).ToString();
+                }
+                else if (operatorhatorozo == "div")
+                {
+                    return (operandus1 / operandus2).ToString();
+                }
+                else if (operatorhatorozo == "mod")
+                {
+                    return ((double)operandus1 % (double)operandus2).ToString();
+                }
+                else
+                {
+                    return "Hibás operátor!";
+                }
+            }
+            catch (Exception)
+            {
+                return "Egyéb hiba!";
+            }
+        }
+    }
+}
--- Operátorok/Program.cs
+++ Operátorok/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Operátorok;
+using System.Collections.Generic;
+
+List<OperatorClass> operatorok = new List<OperatorClass>();
+
+StreamReader sr = new StreamReader("kifejezesek.txt");
+
+while (!sr.EndOfStream)
+{
+    string[] adatok = sr.ReadLine().Split();
+    operatorok.Add(new OperatorClass(int.Parse(adatok[0]), adatok[1], int.Parse(adatok[2])));
+}
+sr.Close();
+
+Console.WriteLine($"2.feladat: Kifejezések száma: {operatorok.Count()}");
+
+Console.WriteLine($"3.feladat: Kifejezések maradékos osztással: {operatorok.Count(o => o.Operatorhatorozo.Contains("mod"))}");
+
+Console.WriteLine(operatorok.Any(o => o.Operandus1 % 10 == 0 && o.Operandus2 % 10 == 0) ? "4.feladat: Van ilyen kifejezés!" : "4.feladat: Nincs ilyen kifejezés!");
+
+string[] osszesoperatortipus = {"+","-","*","/","div","mod"};
+Console.WriteLine($"5.feladat: Statisztika");
+operatorok.Where(o => osszesoperatortipus.Contains(o.Operatorhatorozo)).GroupBy(o => o.Operatorhatorozo).ToList().ForEach(o => Console.WriteLine($"\t{o.Key} -> {o.Count()} db"));
+
+Console.Write($"7.feladat: Kérek egy kifejezést (pl.: 1 + 1): ");
+string megadottertekek = Console.ReadLine();
+string[] megadottertekektomb;
+while (megadottertekek != "vége")
+{
+    megadottertekektomb = megadottertekek.Split();
+    try
+    {
+        Console.WriteLine($"{megadottertekek} = {OperatorClass.Meghatarozas(int.Parse(megadottertekektomb[0]), megadottertekektomb[1], int.Parse(megadottertekektomb[2]))}");
+    }
+    catch (FormatException)
+    {
+        Console.Write($"7.feladat: Kérek egy kifejezést (pl.: 1 + 1): ");
+        megadottertekek = Console.ReadLine();
+        continue;
+    }
+    Console.Write($"7.feladat: Kérek egy kifejezést (pl.: 1 + 1): ");
+    megadottertekek = Console.ReadLine();
+}
+
+StreamWriter sw = new StreamWriter("eredmenyek.txt", append: true);
+operatorok.ForEach(o => sw.WriteLine($"{o.Operandus1} {o.Operatorhatorozo} {o.Operandus2} = {OperatorClass.Meghatarozas(o.Operandus1, o.Operatorhatorozo, o.Operandus1)}"));
+sw.Close();
+Console.Write($"8.feladat: eredmenyek.txt");
